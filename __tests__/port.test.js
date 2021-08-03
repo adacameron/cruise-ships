109e247ca0365230cf9b2056ea1e15491f2d7bda
adacameron <ada.e.cameron@gmail.com>
2021-08-03 11:19:27 +0100

    Add beforeEach() blocks to ship & port tests
    Change cruise-ships.js file name to ships.js

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,29 +1,31 @@
-const Ship = require('../src/ships');
 const Port = require('../src/port');
-const Itinerary = require('../src/itinerary');
+
+let calais;
+let dover;
+let silverShadow;
+let vikingSky;
 
 describe('Port constuctor class', () => {
+    beforeEach(() => {
+        dover = new Port('Dover');
+    });
 
     it('instantiates a new Port object', () => {
-        
-        const port = new Port('Dover');
 
-        expect(port).toBeInstanceOf(Port);
+        expect(dover).toBeInstanceOf(Port);
     });
 
     it('has a name', () => {
-        
-        const port = new Port('Dover');
 
-        expect(port.name).toBe('Dover');
+        expect(dover.name).toBe('Dover');
     });
 });
 
 describe('addShip', () => {
     it('adds the name of a ship when it docks at a port', () => {
 
-        const calais = new Port('Calais');
-        const ship = {};
+        calais = new Port('Calais');
+        ship = {};
 
         calais.addShip(ship);
 
@@ -34,12 +36,9 @@ describe('addShip', () => {
 describe('removeShip', () => {
     it('removes the name of a ship when it sets sail from a port', () => {
 
-        const calais = new Port('Calais');
-        const silverShadow = {}
-        const vikingSky = {}
-
-        // not testing Ship object, so use object literals 
-        // check that a port can store a collection of objects - the interface of the object doesn't matter right now. 
+        calais = new Port('Calais');
+        silverShadow = {}
+        vikingSky = {}
 
         calais.addShip(silverShadow)
         calais.addShip(vikingSky);
--- __tests__/ships.test.js
+++ __tests__/ships.test.js
@@ -3,69 +3,61 @@ const Ship = require('../src/ships');
 const Itinerary = require('../src/itinerary');
 const Port = require('../src/port');
 
-describe('Ship constructor', () => {
-    it('creates a new instance of the ship object', () => {
+let dover;
+let itinerary;
+let ship;
+let calais;
 
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
+describe('Ship constructor', () => {
+   
+    beforeEach(() => {
+        dover = new Port('Dover');
+        itinerary = new Itinerary([dover]);
+        ship = new Ship(itinerary);
+    });
+
+    it('creates a new instance of the ship object', () => {
 
         expect(ship).toBeInstanceOf(Ship);
     });
 
-
     it('gets added to port on instantiation', () => {
-
-        const dover = new Port('Dover');
-        const itinerary = new Itinerary([dover]);
-        const ship = new Ship(itinerary);
-
-        // Port --> Itinerary --> Ship
-        // this instance of Port is made available to the Ship instance 
-        // therefore Port has its addShip() method called
-        // and port.ships contains the Ship instance
-
+        
         expect(dover.ships).toContain(ship);
     });
 });
 
 describe('currentPort', () => {
+    
     it('has a starting port', () => {
-
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-
-        expect(ship.currentPort).toBe(port);
+        dover = new Port('Dover');
+        calais = new Port('Calais');
+        itinerary = new Itinerary([dover, calais]);
+        ship = new Ship(itinerary);
+        expect(ship.currentPort).toBe(dover);
     });
 });
 
 describe('setSail', () => {
+
+    beforeEach(() => {
+        dover = new Port('Dover');
+        calais = new Port('Calais');
+        itinerary = new Itinerary([dover, calais]);
+        ship = new Ship(itinerary);
+    });
+
     it('sets sail from a port', () => {
-
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
-
+       
         ship.setSail();
 
         expect(ship.previousPort).toBe(itinerary.ports[0]);
         expect(ship.currentPort).toBeFalsy();
-
         expect(dover.ships).not.toContain(ship);
-
-        // ship.setSail() should call the currentPort's removeShip() method
-
     });
 
     it('can\'t sail further than its itinerary', () => {
-
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
-
+        
         ship.setSail();
         ship.dock();
 
@@ -76,28 +68,15 @@ describe('setSail', () => {
 describe('dock', () => {
     it('enables the ship to dock at a different port', () => {
 
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
+        dover = new Port('Dover');
+        calais = new Port('Calais');
+        itinerary = new Itinerary([dover, calais]);
+        ship = new Ship(itinerary);
 
         ship.setSail();
         ship.dock();
 
         expect(ship.currentPort).toBe(calais);
         expect(calais.ships).toContain(ship);
-
-        // ports can store ships
-        // to store the ships, the dock() method calls the destination port's addShip() method
-        // i.e. ship.dock() calls calais.addShip() 
-
-        // ship.setSail() 
-        //      sets ship.currentPort is null 
-        //      sets ship.previousPort is Dover
-        //      next port in Itinerary is Calais
-
-        // ship.dock()
-        //      sets ship.currentPort to Calais
-        //      calls port.addShip() 
     });
 });
